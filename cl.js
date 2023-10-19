4096ba88d8398670a7fcdf1df640ce1f8ec32659
mxykxlx <michaellashylle.ong.x@bulsu.edu.ph>
2023-10-19 09:17:20 +0800

    Up

---
+++ cl.js
@@ -0,0 +1,46 @@
+//sa
+function opensa(imageSrc) {
+    var modalsa = document.getElementById('myModalsa');
+    var modalImgsa = document.getElementById('modalImgsa');
+    modalsa.style.display = 'block';
+    modalImgsa.src = imageSrc;
+  }
+  
+  function closeModalsa() {
+    var modal = document.getElementById('myModalsa');
+    modal.style.display = 'none';
+  }
+  
+
+
+
+
+//va
+  function openModalva(imageSrc) {
+    var modalva = document.getElementById('myModalva');
+    var modalImgva = document.getElementById('modalImgva');
+    modalva.style.display = 'block';
+    modalImgva.src = imageSrc;
+  }
+  
+  function closeModalva() {
+    var modalva = document.getElementById('myModalva');
+    modalva.style.display = 'none';
+  }
+
+
+
+
+
+//anm
+function openModalanm(imageSrc) {
+    var modalanm = document.getElementById('myModalanm');
+    var modalImganm = document.getElementById('modalImganm');
+    modalanm.style.display = 'block';
+    modalImganm.src = imageSrc;
+  }
+  
+  function closeModalanm() {
+    var modalanm = document.getElementById('myModalanm');
+    modalanm.style.display = 'none';
+  }
