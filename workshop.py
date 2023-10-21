5cf598642a2c81e528786df30862fc349717fbbf
mxykxlx <michaellashylle.ong.x@bulsu.edu.ph>
2023-10-22 03:03:47 +0800

    Up

---
+++ workshop.py
@@ -0,0 +1,21 @@
+workshop_title = "Unlocking the Power of Python: A Journey through Jupyter Notebooks"
+organizer = "Michaella Shylle Ong"
+date = "October 28, 2023"
+venue = "Online"
+
+print(f"Join us for the {workshop_title} hosted by {organizer}!")
+print(f"Date: {date} | Location: {venue}")
+print("Don't miss this opportunity to:")
+print("1. Learn Python programming from scratch.")
+print("2. Dive into the world of Jupyter Notebooks.")
+print("3. Build exciting projects with Python.")
+print("4. Connect with a community of Python enthusiasts.")
+print("5. Unlock the potential of data science and more!")
+print("Register now and embark on your Python journey.")
+
+
+
+
+
+
+
